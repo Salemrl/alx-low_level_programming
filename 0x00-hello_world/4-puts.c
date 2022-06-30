0b968ecaa3b02ca04a7aba64c62006aa0566bb15
Salem <ladhari.r.s@gmail.com>
2022-06-30 14:33:22 -0700

    ksdjksdjf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
-int main (void)
+
+/**
+*main - starting function
+*Return: Always 0 (Success)
+*/
+
+int main(void)
 {
-        puts ("\"Programming is like building a multiligual puzzle");
-        return (0);
+puts ("\"Programming is like building a multilingual puzzle");
+return (0);
 }
