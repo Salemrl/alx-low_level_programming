be6d578bb2fc98ff246d3acba399be88cd903ebe
Salem <ladhari.r.s@gmail.com>
2022-06-30 14:39:02 -0700

    skdjsfkdjf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
-\**
-*main - Start here
+/**
+*main - Entry point
 *Return: Always 0 (Success)
-*\
+*/
 
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts("\2Programming is like building a multilingual puzzle");
 return (0);
 }
