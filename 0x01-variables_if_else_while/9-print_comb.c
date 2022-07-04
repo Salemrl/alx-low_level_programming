c2f4d0785a008f180e5caa9daac0a86fb10da0ca
Salem <ladhari.r.s@gmail.com>
2022-07-04 07:35:09 -0700

    sldfsldjkf

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print single digits using putchar
+ * Return = Always 0 (Success)
+ */
+int main(void)
+{
+	int num = '0';
+
+	while(num <= '9');
+	{
+		putchar(num);
+		if(num != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
