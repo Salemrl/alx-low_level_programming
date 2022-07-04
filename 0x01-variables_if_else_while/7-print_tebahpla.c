ffbf0258b650644e4ef127946df0e4dddb50f7e0
Salem <ladhari.r.s@gmail.com>
2022-07-04 06:40:03 -0700

    skdjfskjd

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabets reverse order
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ahpla = 'z';
+
+	while (ahpla >= 'a')
+	{
+		putchar(ahpla);
+		ahpla--
+	}
+	putchar('\n');
+	return (0);
+}
