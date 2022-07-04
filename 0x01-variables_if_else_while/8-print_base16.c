2bbf96a2c1f3467f8bc49eda7f74e5d7f4ed5321
Salem <ladhari.r.s@gmail.com>
2022-07-04 07:01:50 -0700

    ksdjfksjd

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - print hexa base
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num = '0';
+	int hexa = 'a';
+
+	while (num <= '9')
+	{
+		putchar(num);
+		num++;
+	}
+
+	while (hexa <= 'f');
+	{
+		putchar(hexa);
+		hexa++;
+	}
+	putchar('\n');
+	return (0);
+}
