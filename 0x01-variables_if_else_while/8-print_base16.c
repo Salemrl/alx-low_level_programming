3338f406015f04c7e3dd3b30760d7f133d8ec174
Salem <ladhari.r.s@gmail.com>
2022-07-04 07:18:03 -0700

    sdkjfksdj

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,19 +10,19 @@ int main(void)
 	int num = '0';
 	int hexa = 'a';
 
-	while (num <= '9') /*print 0-9*/
+while (num <= '9') /*print 0-9*/
 
 	{
 		putchar(num);
 		num++;
 	}
 
-	while (hexa <= 'f'); /*print the rest of the hexabase*/
+while (hexa <= 'f'); /*print the rest of the hexabase*/
 
-	{
+{
 		putchar(hexa);
 		hexa++;
-	}
+}
 
 	putchar('\n');
 	return (0);
