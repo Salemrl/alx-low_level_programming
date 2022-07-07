a68d98767144b44fb64cfa85ea1b0c2269150e01
Salem <ladhari.r.s@gmail.com>
2022-07-07 02:59:24 -0700

    kdjskdf

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "maina.h"
+
+/**
+ * _isdigit - will check if digit
+ * @n: digit to be checked
+ * Return: 1 if digit otherwise 0
+ */
+
+int _isdigit(int n)
+{
+	if(n >= '1' && n <= '9')
+		Return (1);
+
+	return (0);
+}
